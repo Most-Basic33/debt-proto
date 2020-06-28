0bb48ed6f7f2f98d4997bb79e21277f0b3eaa480
bishopwalker <walkerbishop333@gmail.com>
2020-06-28 07:32:28 -0700

    fixed package.json

--- server/index.js
+++ server/index.js
@@ -14,4 +14,4 @@ app.get('/api/debtor', ctrl.totalOwed)//get count of whats of owe
 //Possibly display the time on the card
 
 
-app.listen(port,() =>console.log('server working onport 6660') )
+app.listen(port,() =>console.log('server working onport 6660'))
